cbe2acf9cbaa065eb789ba85dd12ae8495a13fd0
dobragab <dobragab@gmail.com>
2016-05-17 20:42:46 +0200

    main improved

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 #include "CWrapper.hpp"
 
-FILE * copy_file(const FILE *)
+class stdc_error : public std::exception
 {
-    return nullptr;
-}
+    int error_number = errno;
+public:
+    virtual const char * what() const noexcept override
+    {
+        return strerror(error_number);
+    }
+};
 
+// this one is here only to test detecting copy_func.
+//FILE * copy_file(const FILE *)
+//{
+//    return nullptr;
+//}
+
+// This controller class is enough to provide
+// RAII, move semantics, .get() and throws bad_alloc.
 struct FileWrapperBasic
 {
     static constexpr auto ctor_func = fopen;
     static constexpr auto dtor_func = fclose;
-    static constexpr auto copy_func = copy_file;
+
 };
 
+// This class shows the flexibility of CWrapper,
+// using all the features of controller class.
 struct FileWrapperAdvanced
 {
     static FILE* ctor_func(const char* filename)
     {
-        return fopen(filename, "wt");
+        return fopen(filename, "rt");
     }
     static FILE* ctor_func(const char* filename, const char* mode)
     {
@@ -26,17 +42,28 @@ struct FileWrapperAdvanced
     }
 
     static constexpr auto dtor_func = fclose;
+//    static constexpr auto copy_func = copy_file;
+    using exception = stdc_error;
+    static constexpr FILE* invalid_value = nullptr;
+    static constexpr bool validate_func(FILE* ptr)
+    {
+        return ptr != nullptr;
+    }
 };
 
-int main()
+int main() try
 {
-    using FileWrapper = CWrapper<FILE*, FileWrapperBasic>;
+    using FileWrapper = CWrapper<FILE*, FileWrapperAdvanced>;
 
-    FileWrapper file("whatever.txt", "wt");
+    FileWrapper file("you/shall/not/path", "rt");
     fprintf(file.get(), "Hello Unified CWrapper!");
 
-    FileWrapper file2(file);
-    (void)file2;
+//    FileWrapper file2(file);
+//    (void)file2;
 
     return 0;
+}
+catch(std::exception& x)
+{
+    std::cout << x.what();
 }
